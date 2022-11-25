9ef49b0e9fc8accbc230733773bff3526b0bcb40
Andrei <inwaves@live.com>
2022-11-25 14:42:33 -0800

    Processing the data, WIP

--- dtchess/main.py
+++ dtchess/main.py
@@ -1,7 +1,40 @@
+from typing import Tuple
+
+import torch as t
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import GPT2Model, GPT2Tokenizer
+
 from dtchess.utils.utils import parse_args, setup
 
 MAIN = __name__ == "__main__"
+device = "cuda" if t.cuda.is_available() else "cpu"
+
+
+# TODO: add wandb tracking
+
+
+def train(tokeniser: GPT2Tokenizer, model: GPT2Model, optimiser: optim.Adam,
+          dataloaders: Tuple[DataLoader, DataLoader], loss_fn: nn.CrossEntropyLoss, args: dict):
+    train_dl, test_dl = dataloaders
+
+    # Writing a generic training loop for now, update later.
+    for epoch in range(args["num_epochs"]):
+        for X, y in enumerate(tqdm(train_dl)):
+            X, y = X.to(device), y.to(device)
+            preds = model(X)
+            loss = loss_fn(preds, y)
+
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+
+    return model
+
 
 if MAIN:
     args = parse_args()
-    tokeniser, model, optimiser, dataloaders = setup(args)
+    tokeniser, model, optimiser, dataloaders, loss_fn = setup(args)
+    trained_model = train(tokeniser, model, optimiser, dataloaders, loss_fn, args)
--- dtchess/utils/utils.py
+++ dtchess/utils/utils.py
@@ -1,11 +1,14 @@
-import dtchess as dt
-import torch.optim as optim
-import torch.nn as nn
+import os
+from argparse import ArgumentParser
+from typing import Tuple
 
+import chess.pgn as pgn
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
 from transformers import GPT2Model, GPT2Tokenizer
-from typing import Tuple
-from argparse import ArgumentParser
+
+import dtchess as dt
 
 
 def count_parameters(model: nn.Module) -> int:
@@ -15,20 +18,34 @@ def count_parameters(model: nn.Module) -> int:
 def parse_args() -> dict:
     parser = ArgumentParser()
 
-    # Use this pattern to control args...
+    # TODO: Use this pattern to control args...
     # parser.add_argument()
 
     argspace = parser.parse_args()
     return vars(argspace)
 
 
-def setup(args: dict) -> Tuple[GPT2Tokenizer, GPT2Model, optim.Adam, Tuple[DataLoader, DataLoader]]:
+def setup(args: dict) -> Tuple[
+    GPT2Tokenizer, GPT2Model, optim.Adam, Tuple[DataLoader, DataLoader], nn.CrossEntropyLoss]:
     tokeniser, model = dt.models.create_model()
     optimiser = optim.Adam(model.parameters, lr=args["lr"])
-    dataloaders = preprocess_data(args)
+    dataloaders = preprocess_data(tokeniser, args)
+    loss_fn = nn.CrossEntropyLoss
 
-    return tokeniser, model, optimiser, dataloaders
+    return tokeniser, model, optimiser, dataloaders, loss_fn
 
 
-def preprocess_data(args: dict) -> Tuple[DataLoader, DataLoader]:
-    pass
+def preprocess_data(tokeniser: GPT2Tokenizer, args: dict) -> Tuple[DataLoader, DataLoader]:
+    # TODO: how to preprocess .PGN data?
+    print(os.getcwd())
+    pgn_file = open("./dtchess/data/sample.pgn")
+    game = pgn.read_game(pgn_file)
+    all_nodes = []
+    while game.next():
+        next_node = game.next()
+        all_nodes.append(next_node)
+        game = next_node
+    moves_and_evals = [(node.move.uci(), node.eval().relative.cp) for node in all_nodes]
+    print(moves_and_evals)
+
+    return None, None
