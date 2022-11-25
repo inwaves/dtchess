a061edd8dd750f451dabb56b2bbea25ca562f0ee
Andrei <inwaves@live.com>
2022-11-24 16:30:27 -0800

    Refactor structure so I can use package

---
+++ dtchess/main.py
@@ -0,0 +1,6 @@
+import models.decision_transformer as dt
+
+MAIN = __name__ == "__main__"
+
+if MAIN:
+    dt.create_model()
--- dtchess/models/decision_transformer.py
+++ dtchess/models/decision_transformer.py
@@ -1,15 +1,13 @@
+from dtchess.utils.utils import count_parameters
 from transformers import GPT2Tokenizer, GPT2Model
 import torch as t
 
 
-if __name__ == "__main__":
+def create_model():
     # gpt2 is 1.5B parameters, gpt2-medium is 355m.
     device = "cuda" if t.cuda.is_available() else "cpu"
     tokeniser = GPT2Tokenizer.from_pretrained("gpt2-medium")
     model = GPT2Model.from_pretrained("gpt2-medium")
 
-    # Prediction: this isn't going to fit on my device.
     model = model.to(device)
-
-    modely
     print(model)
