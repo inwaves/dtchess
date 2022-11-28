9e1dac5baca68b73e69d77bb1947cb5589293fb7
Andrei <inwaves@live.com>
2022-11-27 17:21:40 -0800

    Add logic to process one game into a sequence

--- dtchess/utils/utils.py
+++ dtchess/utils/utils.py
@@ -1,10 +1,10 @@
-import os
 from argparse import ArgumentParser
-from typing import Tuple
+from typing import Tuple, Optional
 
 import chess.pgn as pgn
 import torch.nn as nn
 import torch.optim as optim
+from chess.pgn import ChildNode
 from torch.utils.data import DataLoader
 from transformers import GPT2Model, GPT2Tokenizer
 
@@ -35,33 +35,73 @@ def setup(args: dict) -> Tuple[
     return tokeniser, model, optimiser, dataloaders, loss_fn
 
 
-def preprocess_data(tokeniser: GPT2Tokenizer, args: dict) -> Tuple[DataLoader, DataLoader]:
+def process_game(game: pgn.Game, sequence_type: str) -> str:
     """Converts PGN data into sequences usable by our model, then wraps them in DataLoaders.
-       This should allow the use of CLI args to switch between different modes of parsing the file:
-        - convert to "[<ELO>|<OUTCOME>|<EVALS>] move1 move2 ..."
-        - convert to "[<ELO>|<OUTCOME>|<EVALS>] board1 board2 ..."
+       Allows the use of CLI args to switch between different modes of parsing the file:
+        - "[<ELO>|<RESULT>|<RETURN>] move1 move2 ..."
+        - "[<ELO>|<RESULT>|<RETURN>] board1 board2 ..."
     """
-    pgn_file = open("./dtchess/data/sample.pgn")
-    game = pgn.read_game(pgn_file)
+    body, header = "", ""
     elo = game.headers["WhiteElo"]
-    result= game.headers["Result"]
-    all_nodes = []
+    result = game.headers["Result"]
+
+    # Play the game until the end, remembering moves, evals and board states.
+    all_nodes, boards = [], []
+    board = game.board()
     while game.next():
-        next_node = game.next()
+        # We don't remember the initial board state, it's always the same.
+        board.push(game.mainline_moves()[0])
+        boards.append(board.copy())
+
+        next_node: Optional[ChildNode] = game.next()
         all_nodes.append(next_node)
         game = next_node
     moves = [node.move.uci() for node in all_nodes]
     evals = [node.eval().relative.cp for node in all_nodes]
-    white_total_loss = sum(evals[::2])
-    black_total_loss = sum(evals[1::2])
-    sequence = ""
-    if args.sequence_type == "full":
-        sequence = f"<ELO>{elo}</ELO> <RES>{result}</RES> <RET>{white_total_loss}</RET> {' '.join(moves)}"
-    elif args.sequence_type == "elo":
-        sequence = f"<ELO>{elo}</ELO> {' '.join(moves)}"
-    elif args.sequence_type == "result":
-        sequence = f"<RES>{result}</RES> {' '.join(moves)}"
-        
 
+    # A sequence comprises a header and a body.
+    # The header contains some combination of ELO, game result and total return tokens.
+    # The body is either a sequence of moves or boards, with or without the evals of these from a chess engine.
+    header_type, body_type = sequence_type.split(":")
+    if header_type == "full":
+        # "<ELO>[ELO]</ELO> <RES>[RES]</RES> <RET>[RETURN]</RET> [MOVE1] [MOVE2] ..."
+        white_total_loss = sum(evals[::2])
+        header = f"<ELO>{elo}</ELO> <RES>{result}</RES> <RET>{white_total_loss}</RET>"
+    elif header_type == "elo":
+        # "<ELO>[ELO]</ELO> [MOVE1] [MOVE2] ..."
+        header = f"<ELO>{elo}</ELO>"
+    elif header_type == "result":
+        # "<RES>[RESULT]</RES> [MOVE1] [MOVE2] ..."
+        header = f"<RES>{result}</RES>"
+
+    # Header can be blank if we want to feed in just the board/move with or without evals.
+    elif body_type == "moves":
+        # "[MOVE1] [MOVE2] ...".
+        body = f"{' '.join(moves)}"
+    elif body_type == "boards":
+        # "[BOARD1] [BOARD2] ...
+        body = f"{' '.join(boards)}"
+    elif body_type == "evalmoves":
+        # "[MOVE1] [EVAL1] [MOVE2] [EVAL2] ...".
+        moves_evals_sequence = ' '.join(list(zip(moves, evals)))
+        body = f"{' '.join(moves_evals_sequence)}"
+    elif sequence_type == "evalboards":
+        # "[BOARD1] [EVAL1] [BOARD2] [EVAL2] ...".
+        boards_evals_sequence = ' '.join(list(zip(boards, evals)))
+        body = f"{' '.join(boards_evals_sequence)}"
+
+    return header + body
+
+
+def preprocess_data(tokeniser: GPT2Tokenizer, args: dict) -> Tuple[DataLoader, DataLoader]:
+    """Preprocesses data for the decision transformer."""
+    # TODO: Add logic here to crawl all the files.
+
+    pgn_file = open("./dtchess/data/sample.pgn")
+    while game := pgn.read_game(pgn_file) is not None:
+        sequence = process_game(game, args["sequence_type"])
+
+    # TODO: How to stream this data?
+    # TODO: Add logic here to tokenise the sequences.
+    # train_dl, test_dl = DataLoader(train_ds, batch_size=args["batch_size"]), DataLoader(test_ds, batch_size=args["batch_size"])
     return None, None
-
