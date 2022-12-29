84d663a233954a78aa6a8e66238975af8e145179
Andrei Alexandru <inwaves@live.com>
2022-12-29 15:40:28 -0800

    Add LM head model

--- dtchess/models/gpt.py
+++ dtchess/models/gpt.py
@@ -1,14 +1,14 @@
 import torch as t  # type: ignore
 import torch.nn as nn  # type: ignore
-from transformers import GPT2Tokenizer, GPT2Model  # type: ignore
+from transformers import GPT2Tokenizer, GPT2LMHeadModel  # type: ignore
 
 
-def create_model(model_type: str = "gpt2-medium") -> tuple[GPT2Model, GPT2Tokenizer]:
+def create_model(model_type: str = "gpt2-medium") -> tuple[GPT2LMHeadModel, GPT2Tokenizer]:
     # gpt2 is 1.5B parameters, gpt2-medium is 355m.
     device = "cuda" if t.cuda.is_available() else "cpu"
     tokeniser = GPT2Tokenizer.from_pretrained(model_type)
     tokeniser.add_special_tokens({"pad_token": "[PAD]"})
-    model = GPT2Model.from_pretrained(model_type)
+    model = GPT2LMHeadModel.from_pretrained(model_type)
 
     # Modifying token embedding since we added a new token type...
     model.wte = nn.Embedding(tokeniser.vocab_size + 1, model.wte.embedding_dim)
